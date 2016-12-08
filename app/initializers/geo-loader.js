72aaab9f13b4fe675fe2bf06b7e35d51397e5124
Doug DeBold <ddebold56@gmail.com>
2016-12-08 14:30:10 -0600

    Got fast boot working

--- app/initializers/geo-loader.js
+++ app/initializers/geo-loader.js
@@ -1,16 +1,17 @@
 export function initialize(application) {
-    // application.deferReadiness()
-    // const geo = navigator.geolocation;
-    // geo.getCurrentPosition((pos) => {
-    //     let pt = pos.coords;
-    //     let loc = { lat: Math.round(pt.latitude), lng: Math.round(pt.longitude) };
-    //     application.register('data:location', loc, { instantiate: false });
+    console.log('fastboot')
+    if (typeof(FastBoot) === 'undefined') {
+      console.log('not fastboot');
+      application.deferReadiness();
+      const geo = navigator.geolocation;
+      geo.getCurrentPosition((pos) => {
+          let pt = pos.coords;
+          let loc = { lat: Math.round(pt.latitude), lng: Math.round(pt.longitude) };
+          application.register('data:location', loc, { instantiate: false });
 
-    //     application.advanceReadiness();
-    // })
-
-    let loc = { lat: 43, lng: -93 };
-    application.register('data:location', loc, { instantiate: false });
+          application.advanceReadiness();
+      });
+    }
 }
 
 export default {
